8aa558d15f69d1945353f2020dd349c6223b04af
FedericoVelez25 <federico.velezferro@gmail.com>
2022-09-20 21:56:19 +0200

    firebase installed

--- bigportfolio/functions/index.js
+++ bigportfolio/functions/index.js
@@ -1,9 +1,14 @@
 const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const nodemailer = requite("nodemailer");
+admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+let transporter = nodemailer.createTransport({
+  host: "smtp.example.com",
+  port: 887,
+  secure: false,
+  auth: {
+    user: "username",
+    pass: "password",
+  },
+});
